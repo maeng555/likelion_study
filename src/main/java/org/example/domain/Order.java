94a8399e840b17f446a2058113b03211adccb084
maeng555 <mjy5188@naver.com>
2025-03-16 17:27:58 +0900

    Chore: Order - totalAmount,userId 변수 변환

--- src/main/java/org/example/domain/Order.java
+++ src/main/java/org/example/domain/Order.java
@@ -4,13 +4,9 @@ import java.util.Map;
 
 public class Order {
     private final Map<String, Integer> items;
-    private final int totalAmount;
-    private final int userId;
 
     public Order(Map<String, Integer> items, int totalAmount, int userId) {
         this.items = items;
-        this.totalAmount = totalAmount;
-        this.userId = userId;
     }
 
     public Map<String, Integer> getItems() {
